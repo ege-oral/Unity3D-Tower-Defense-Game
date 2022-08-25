95efa071d03233d80f49be82a1d3921217e8c17d
Ege Oral <egeoral@gmail.com>
2022-08-25 20:03:40 +0300

    Aim function added.

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class TargetLocator : MonoBehaviour
 {
+
+    [SerializeField] Transform weapon;
+    [SerializeField] Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = FindObjectOfType<EnemyMovement>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AimWeapon();
+    }
+
+    private void AimWeapon()
+    {
+        weapon.LookAt(target);
+        //weapon.transform.LookAt(target);
     }
 }
